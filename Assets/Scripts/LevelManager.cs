f76709c73362ae66ffc037ba181e28f296cb2312
Корниевский Никита <78549055+Nikita-bip@users.noreply.github.com>
2024-10-01 11:06:53 +0300

    commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    #region Singleton
+    public static LevelManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+    public Vector3 startPosition;
+    public Grid grid;
+    public List<GameObject> bubblesPrefabs;
+    public Transform bubblesArea;
+    public GameObject ballPrefab1;
+    public GameObject ballPrefab2;
+    public GameObject ballPrefab3;
+    public GameObject ballPrefab4;
+
+    public string filePath = "Assets/Resources/Level.txt"; // Путь к текстовому файлу
+    public Dictionary<char, GameObject> prefabDictionary;
+    public List<GameObject> bubblesInScene;
+    public List<string> colorsInScene;
+
+
+    private void Start()
+    {
+        prefabDictionary = new Dictionary<char, GameObject>
+    {
+        { '1', ballPrefab1 },
+        { '2', ballPrefab2 },
+        { '3', ballPrefab3 },
+        { '4', ballPrefab4 },
+    };
+
+        grid = GetComponent<Grid>();
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        GenerateField(startPosition);
+
+        SnapChildrensToGrid(bubblesArea);
+        UpdateListOfBubblesInScene();
+        GameManager.instance.shootScript.CreateNewBubbles();
+    }
+
+    public void GenerateField(Vector3 startPosition)
+    {
+        string[] lines = File.ReadAllLines(filePath);
+
+        for (int y = 0; y < lines.Length; y++)
+        {
+            string line = lines[y];
+
+            for (int x = 0; x < line.Length; x++)
+            {
+                char currentChar = line[x];
+                if (prefabDictionary.ContainsKey(currentChar)) // Проверка наличия префаба для текущего символа
+                {
+                    Vector3 position = new Vector3(x * 25, -y * 25, 0) + startPosition; // Позиция объекта
+                    GameObject prefab = prefabDictionary[currentChar]; // Получение префаба из словаря
+                    Instantiate(prefab, position, Quaternion.identity, bubblesArea);
+                    Debug.Log($"Spawned {currentChar} at {position}");
+                }
+            }
+        }
+    }
+
+    #region Snap to Grid
+    private void SnapChildrensToGrid(Transform parent)
+    {
+        foreach (Transform t in parent)
+        {
+            SnapToNearestGripPosition(t);
+        }
+    }
+
+    public void SnapToNearestGripPosition(Transform t)
+    {
+        Vector3Int cellPosition = grid.WorldToCell(t.position);
+        t.position = grid.GetCellCenterWorld(cellPosition);
+        t.rotation = Quaternion.identity;
+
+    }
+    #endregion
+
+    public void UpdateListOfBubblesInScene()
+    {
+        List<string> colors = new List<string>();
+        List<GameObject> newListOfBubbles = new List<GameObject>();
+
+        foreach (Transform t in bubblesArea)
+        {
+            Bubble bubbleScript = t.GetComponent<Bubble>();
+            if (colors.Count < bubblesPrefabs.Count && !colors.Contains(bubbleScript.bubbleColor.ToString()))
+            {
+                string color = bubbleScript.bubbleColor.ToString();
+
+                foreach (GameObject prefab in bubblesPrefabs)
+                {
+                    if (color.Equals(prefab.GetComponent<Bubble>().bubbleColor.ToString()))
+                    {
+                        colors.Add(color);
+                        newListOfBubbles.Add(prefab);
+                    }
+                }
+            }
+        }
+
+        colorsInScene = colors;
+        bubblesInScene = newListOfBubbles;
+    }
+    public void SetAsBubbleAreaChild(Transform bubble)
+    {
+        SnapToNearestGripPosition(bubble);
+        bubble.SetParent(bubblesArea);
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+	public bool canShoot;
+	public float speed = 25f;
+
+	public Transform nextBubblePosition;
+	public GameObject currentBubble;
+	public GameObject nextBubble;
+	public GameObject bottomShootPoint;
+
+	private Vector2 lookDirection;
+	private float lookAngle;
+	private GameObject line;
+	private GameObject limit;
+	private LineRenderer lineRenderer;
+	private Vector2 gizmosPoint;
+
+	public void Awake()
+	{
+		line = GameObject.FindGameObjectWithTag("Line");
+		limit = GameObject.FindGameObjectWithTag("Limit");
+	}
+
+	public void Update()
+	{
+			gizmosPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+			lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+
+			if (canShoot
+				&& Input.GetMouseButtonUp(0)
+				&& (Camera.main.ScreenToWorldPoint(Input.mousePosition).y > bottomShootPoint.transform.position.y)
+				&& (Camera.main.ScreenToWorldPoint(Input.mousePosition).y < limit.transform.position.y))
+			{
+				canShoot = false;
+				Shoot();
+			}
+	}
+
+	public void Shoot()
+	{
+		if (currentBubble == null) CreateNextBubble();
+		ScoreManager.GetInstance().AddThrows();
+		transform.rotation = Quaternion.Euler(0f, 0f, lookAngle - 90f);
+		currentBubble.transform.rotation = transform.rotation;
+		currentBubble.GetComponent<CircleCollider2D>().enabled = true;
+		Rigidbody2D rb = currentBubble.GetComponent<Rigidbody2D>();
+		rb.AddForce(currentBubble.transform.up * speed, ForceMode2D.Impulse);
+		rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+		rb.gravityScale = 0;
+		currentBubble = null;
+	}
+
+	public void CreateNewBubbles()
+	{
+		if (nextBubble != null)
+			Destroy(nextBubble);
+
+		if (currentBubble != null)
+			Destroy(currentBubble);
+
+		nextBubble = null;
+		currentBubble = null;
+		CreateNextBubble();
+		canShoot = true;
+	}
+
+	public void CreateNextBubble()
+	{
+		List<GameObject> bubblesInScene = LevelManager.instance.bubblesInScene;
+		List<string> colors = LevelManager.instance.colorsInScene;
+
+		if (bubblesInScene.Count < 1) return;
+
+		if (nextBubble == null)
+		{
+			nextBubble = InstantiateNewBubble(bubblesInScene);
+		}
+
+		if (currentBubble == null)
+		{
+			currentBubble = nextBubble;
+			currentBubble.transform.position = transform.position;
+			nextBubble = InstantiateNewBubble(bubblesInScene);
+		}
+	}
+
+	private GameObject InstantiateNewBubble(List<GameObject> bubblesInScene)
+	{
+		if (bubblesInScene.Count > 0)
+		{
+			GameObject newBubble = Instantiate(bubblesInScene[Random.Range(0, bubblesInScene.Count)]);
+			newBubble.transform.position = nextBubblePosition.position;
+			newBubble.GetComponent<Bubble>().isFixed = false;
+			newBubble.GetComponent<CircleCollider2D>().enabled = false;
+			Rigidbody2D rb2d = newBubble.AddComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+			rb2d.gravityScale = 0f;
+			return newBubble;
+		}
+		else
+		{
+			return null;
+		}
+
+	}
+}
